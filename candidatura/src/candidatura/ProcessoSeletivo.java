6190a2e11ffd2f7f699a184a05a901ab7c76c8ce
magnomenezes <magnomenezes007@gmail.com>
2024-05-30 18:04:41 -0300

    Projeto candidatura

---
+++ candidatura/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,21 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		analisarCandidato(1900.0);
+		analisarCandidato(2200.0);
+		analisarCandidato(2000.0);
+
+	}
+	static void analisarCandidato (double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if(salarioBase>salarioPretendido) {
+			System.out.println("Ligar para o candidato");
+		}else if(salarioBase == salarioPretendido)
+			System.out.println("Ligar para o candidato com um contraproposta.");
+	else { 
+		System.out.println("Aguardando resultado dos demais.");
+	}
+	}
+}
